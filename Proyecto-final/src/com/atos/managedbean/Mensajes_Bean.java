d2b8479f8220ddc189bad6296e6bb434464fb86b
Cguilled <guillermo6795@gmail.com>
2018-08-08 14:08:54 +0200

    Más mierdas en la vista e intento de hacer growl messages.

---
+++ Proyecto-final/src/com/atos/managedbean/Mensajes_Bean.java
@@ -0,0 +1,91 @@
+package com.atos.managedbean;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean(name = "mensajes_bean")
+@CustomScoped(value = "#{window}")
+public class Mensajes_Bean implements Serializable {
+
+	// Propiedades del bean
+	private String autoHide = "true";
+	private boolean closeAll = true;
+	private int maxVisibleMessages = 0;
+	private String position = "top-right";
+	private String header = "";
+
+	private boolean error;
+
+	// Getters y setters
+	public String getAutoHide() {
+		return autoHide;
+	}
+
+	public void setAutoHide(String autoHide) {
+		this.autoHide = autoHide;
+	}
+
+	public boolean isCloseAll() {
+		return closeAll;
+	}
+
+	public void setCloseAll(boolean closeAll) {
+		this.closeAll = closeAll;
+	}
+
+	public int getMaxVisibleMessages() {
+		return maxVisibleMessages;
+	}
+
+	public void setMaxVisibleMessages(int maxVisibleMessages) {
+		this.maxVisibleMessages = maxVisibleMessages;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public boolean getError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
+
+	public void listener(ActionEvent event) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		// Eliminar mensajes existentes
+		Iterator<FacesMessage> i = facesContext.getMessages();
+		while (i.hasNext()) {
+			i.next();
+			i.remove();
+		}
+
+		// Anadir mensajes
+		UIComponent component = event.getComponent();
+
+		String message = "Erroooooorrrrrr";
+		FacesContext.getCurrentInstance().addMessage(message, new FacesMessage("Iniciando sesión..."));
+	}
+}
