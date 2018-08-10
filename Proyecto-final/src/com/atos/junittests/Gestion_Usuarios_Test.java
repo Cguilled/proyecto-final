e78d295fa514aabaf2f5ce3d3eb2bf1a018eaf55
JavierGD <omega.19994@gmail.com>
2018-08-10 15:15:47 +0200

    testing para gestion de usuarios, esta menos roto,

--- Proyecto-final/src/com/atos/junittests/Gestion_Usuarios_Test.java
+++ Proyecto-final/src/com/atos/junittests/Gestion_Usuarios_Test.java
@@ -2,45 +2,66 @@ package com.atos.junittests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.atos.hibernate.dto.Roles;
+import com.atos.hibernate.dto.Usuarios;
+import com.atos.hibernate.modelo.IGestion_Usuarios;
 
 public class Gestion_Usuarios_Test {
+	private Usuarios defaultUser = new Usuarios(1, "0", "0", "0", "0", "0", null, "false");
+	private IGestion_Usuarios gestion;
+	private ApplicationContext ctx;
 	
+	public Gestion_Usuarios_Test() {
+		ctx = new ClassPathXmlApplicationContext("com/atos/spring/modelo.xml");
+	}
 	
+	@Before
+	public void restartGestion() {
+		gestion = ctx.getBean(IGestion_Usuarios.class);
+	}
 
 	@Test
 	public void testConsultar_PorIdNombre() {
-		fail("Not yet implemented");
+		Usuarios got;
+		gestion.alta_Usuario(defaultUser);
+		got = gestion.consultar_PorIdNombre(defaultUser.getId_Usuario());
+		gestion.baja_Usuario(got);
+		assertEquals(defaultUser.getDAS(), got.getDAS());
 	}
 
 	@Test
 	public void testConsultar_Todos() {
-		fail("Not yet implemented");
+		List<Usuarios> got;
+		gestion.alta_Usuario(defaultUser);
+		got = gestion.consultar_Todos();
+		gestion.baja_Usuario(defaultUser);
+		assertEquals(got, 1);
 	}
 
 	@Test
 	public void testConsultar_PorClaveYDAS() {
-		fail("Not yet implemented");
+		Usuarios got;
+		gestion.alta_Usuario(defaultUser);
+		gestion.baja_Usuario(defaultUser);
+		got = gestion.consultar_PorClaveYDAS(defaultUser.getDAS(), defaultUser.getPassword());
+		assertEquals(got.getDAS(), defaultUser.getDAS());
 	}
-
-	@Test
-	public void testAlta_Usuario() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBaja_Usuario() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testModificacion_Usuario() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetUsuario_dao() {
-		fail("Not yet implemented");
+		Usuarios modifiedUser = defaultUser;
+		modifiedUser.setPassword("22");
+		gestion.alta_Usuario(defaultUser);
+		gestion.modificacion_Usuario(modifiedUser);
+		assertEquals(gestion.consultar_PorIdNombre(defaultUser.getId_Usuario()).getPassword(), modifiedUser.getPassword());
+		gestion.baja_Usuario(defaultUser);
 	}
 
 }
