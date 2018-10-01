55bd86b0b8dc2c10498378740a6932e8e4c5e90f
Cguilled <guillermo6795@gmail.com>
2018-10-01 17:09:57 +0200

    Time out funcionando sin mensajes, falta logout.

---
+++ ManolitoSecurity/src/com/atos/springSecurity/CustomLogoutSuccessHandler.java
@@ -0,0 +1,37 @@
+package com.atos.springSecurity;
+
+import java.io.IOException;
+
+import javax.faces.bean.ManagedBean;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+	@RequestMapping(value="/logout", method = RequestMethod.POST)
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		if (authentication != null && authentication.getDetails() != null) {
+			try {
+				request.getSession().invalidate();
+				System.out.println("User Successfully Logout");
+				// you can add more codes here when the user successfully logs out,
+				// such as updating the database for last active.
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		// redirect to login
+		response.sendRedirect("/login.jsp");
+	}
+
+}
